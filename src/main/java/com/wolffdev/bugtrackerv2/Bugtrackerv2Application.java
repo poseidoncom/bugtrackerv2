922b165cbc8e2f87f3c7f8b5eea4b2c5fd542a0d
Tuukka <hartikainentuukka@gmail.com>
2022-02-07 23:35:10 +0200

    lisätään RESTful palvelu. Päivityksiä palveluun

--- src/main/java/com/wolffdev/bugtrackerv2/Bugtrackerv2Application.java
+++ src/main/java/com/wolffdev/bugtrackerv2/Bugtrackerv2Application.java
@@ -1,17 +1,12 @@
 package com.wolffdev.bugtrackerv2;
 
 
+import com.wolffdev.bugtrackerv2.domain.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import com.wolffdev.bugtrackerv2.domain.Bug;
-import com.wolffdev.bugtrackerv2.domain.BugRepository;
-import com.wolffdev.bugtrackerv2.domain.Developer;
-import com.wolffdev.bugtrackerv2.domain.DeveloperRepository;
-
 
 
 @SpringBootApplication
@@ -19,27 +14,38 @@ public class Bugtrackerv2Application {
 	@Autowired
 	private BugRepository repository;
 	@Autowired
-	private DeveloperRepository derepository;
+	private DeveloperRepository drepository;
+	@Autowired
+	private UserRepository urepository;
 
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bugtrackerv2Application.class, args);
 	}
-	@Bean
 
+	@Bean
 	CommandLineRunner runner(){
 		return args -> {
 			Developer developer1 = new Developer("Tuukka", "Hartikainen");
 			Developer developer2 = new Developer("Perttu", "Matikainen");
-			derepository.save(developer1);
-			derepository.save(developer2);
+			drepository.save(developer1);
+			drepository.save(developer2);
 
 
 			repository.save(new Bug("UI ois jees", "Vastaanotettu", 1,3, developer1));
 			repository.save(new Bug("Tunnistautuminen tarvitaan", "Vastaanotettu", 1,3, developer2));
 			repository.save(new Bug("bugiin liitetyn devaajan tietojen haussa ongelmisa restapin kautta", "Vastaanotettu", 3,5, developer2));
 
+			// username: user password: tuukka
+			urepository.save(new User("user",
+					"$2a$10$E0zTmZZT2omhBgv2tLRjcu.azKZunxq6h4JW3yrXFaiRzkTfwKDQC",
+					"USER"));
+			// username: admin password: admin
+			urepository.save(new User("admin",
+					"$2a$04$KNLUwOWHVQZVpXyMBNc7JOzbLiBjb9Tk9bP7KNcPI12ICuvzXQQKG",
+					"ADMIN"));
 		};
+
 	}
 
 }
--- src/main/java/com/wolffdev/bugtrackerv2/domain/BugRepository.java
+++ src/main/java/com/wolffdev/bugtrackerv2/domain/BugRepository.java
@@ -1,6 +1,16 @@
 package com.wolffdev.bugtrackerv2.domain;
+
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-
+@RepositoryRestResource
 public interface BugRepository extends CrudRepository <Bug, Long> {
+
+    List<Bug> findByCriticality(@Param("criticality") int criticality);
+
+    // Fetch cars by color
+    List<Bug> findByDescription(@Param("description") String description);
 }
